b931ed7d345fbc26de412ef892b733dc1cf6ed77
Marvin Lee <marvinlee.github@gmail.com>
2023-04-19 21:57:26 -0700

    Handles invalid JSON in body

--- src/app.js
+++ src/app.js
@@ -3,7 +3,29 @@ import initialRouter from "./routes/initial-router.js";
 import apiRouter from "./routes/api-router.js";
 
 const app = express();
-app.use(express.json());
+app.use((req, res, next) => {
+	express.json()(req, res, err => {
+		if(err && err.type === "entity.parse.failed") {
+			return res.status(400).send({
+				ok: false,
+				error: "invalid-json-body"
+			});
+		}
+
+		if (err) {
+			console.warn("Unknown JSON/Body Parsing Error:");
+			console.error(err);
+
+			return res.status(400).send({
+				ok: false,
+				error: "unknown-body-parse-error",
+				code: err.type
+			});
+		}
+
+		next();
+	});
+});
 // Is swapped to apiRouter once the startup process is completed
 let router = initialRouter;
 
