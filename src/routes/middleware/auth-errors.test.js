4ae7b2ee387e9963c5a22031ffa100b8312af420
Marvin Lee <marvinlee.github@gmail.com>
2023-04-22 23:04:18 -0700

    Now checks valid auth in tests

--- src/routes/middleware/auth-errors.test.js
+++ src/routes/middleware/auth-errors.test.js
@@ -32,5 +32,33 @@ describe("Auth Middleware", () => {
 			})
 		);
 		expect(next).not.toHaveBeenCalled();
+	});
+
+	it("Handles Valid User Auth", () => {
+		const { noAuth: authExistsMiddleware } = require("./auth-errors.js").default;
+
+		const req = {};
+		req.header = jest.fn(headerName => {
+			if(headerName.toLowerCase() === "authorization") {
+				return "Bearer U-User-A-Key"
+			}
+
+			return undefined;
+		});
+
+		const res = {};
+		res.send = jest.fn();
+		res.status = jest.fn(() => res);
+
+		const next = jest.fn();
+
+		authExistsMiddleware(req, res, next);
+		expect(res.status).not.toBeCalled();
+		expect(res.send).not.toBeCalled();
+		expect(next).toHaveBeenCalled();
+		console.log(req.locals)
+		expect(req.locals).toMatchObject({
+			apiKey: "U-User-A-Key"
+		})
 	})
 });
