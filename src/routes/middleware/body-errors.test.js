c5df249ba835043175acf3e1eb8e976c030bb439
Marvin Lee <marvinlee.github@gmail.com>
2023-04-28 14:37:57 -0700

    Tests written for body-errors.js

--- src/routes/middleware/body-errors.test.js
+++ src/routes/middleware/body-errors.test.js
@@ -1,5 +1,3 @@
-import { noBodyErrors } from "./body-errors.js";
-
 describe("No Empty Body Middleware", () => {
 	it("Handles Empty Body Error", () => {
 		const { noBodyErrors } = require("./body-errors.js");
@@ -31,3 +29,91 @@ describe("No Empty Body Middleware", () => {
 		expect(next).not.toHaveBeenCalled();
 	});
 });
+
+describe("Require Specific Body Keys Middleware", () => {
+	it("Pass All Keys Exist", () => {
+		const { ensureBodyKeys } = require("./body-errors.js");
+		const ensureKeyMiddleware = ensureBodyKeys(["a", "b", "c"])[1];
+
+		const req = {};
+		req.body = {
+			a: "something",
+			b: "something",
+			c: "something",
+		};
+
+		const res = {};
+		res.send = jest.fn();
+		res.status = jest.fn(() => res);
+
+		const next = jest.fn();
+
+		ensureKeyMiddleware(req, res, next);
+		expect(res.status).not.toBeCalled();
+		expect(res.send).not.toBeCalled();
+		expect(next).toBeCalled();
+	});
+
+	it("Rejects If Missing Keys", () => {
+		const { ensureBodyKeys } = require("./body-errors.js");
+		const ensureKeyMiddleware = ensureBodyKeys(["a", "b", "c"])[1];
+
+		const req = {};
+		req.body = {
+			a: "something",
+			c: "something",
+		};
+
+		const res = {};
+		res.send = jest.fn();
+		res.status = jest.fn(() => res);
+
+		const next = jest.fn();
+
+		ensureKeyMiddleware(req, res, next);
+		expect(res.status).toBeCalledWith(400);
+		expect(res.send).toBeCalledWith(
+			expect.objectContaining({
+				ok: false,
+				error: "body_missing_fields",
+				missing_fields: ["b"]
+			})
+		);
+		expect(next).not.toHaveBeenCalled();
+	});
+
+	it("Handles Custom Errors", () => {
+		const { ensureBodyKeys } = require("./body-errors.js");
+		const ensureKeyMiddleware = ensureBodyKeys(["a", "b", "c"], {
+			b: {
+				error: {
+					ok: false,
+					error: "b_missing"
+				}
+			}
+		})[1];
+
+		const req = {};
+		req.body = {
+			a: "something",
+			c: "something",
+		};
+
+		const res = {};
+		res.send = jest.fn();
+		res.status = jest.fn(() => res);
+
+		const next = jest.fn();
+
+		ensureKeyMiddleware(req, res, next);
+		expect(res.status).toBeCalledWith(400);
+		expect(res.send).toBeCalledWith(
+			expect.objectContaining({
+				ok: false,
+				error: "b_missing"
+			})
+		);
+		expect(next).not.toHaveBeenCalled();
+	});
+});
+
