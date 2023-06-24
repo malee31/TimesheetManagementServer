724bf72684481f1b947d218e322745e6af0ffb9c
Marvin Lee <marvinlee.github@gmail.com>
2023-06-23 23:48:09 -0700

    Added plan for test db (Short)

--- src/database/__mocks__/database-interface.js
+++ src/database/__mocks__/database-interface.js
@@ -3,4 +3,10 @@ const mockedDatabaseInterface = require("../../test/mock-database-interface.js")
 const mockedDBI = jest.createMockFromModule("../database-interface.js");
 Object.assign(mockedDBI, mockedDatabaseInterface);
 
+// TODO: Complete list of tasks
+// - Create global setup and teardown for database
+// - Insert fixtures into testing database
+// - Consider unique tables for each instance with a mock function (Hard)
+//   - Maybe implement by cloning a base table/dataset
+
 module.exports = mockedDBI;
