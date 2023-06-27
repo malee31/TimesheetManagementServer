0387cb50c9802c7f9528efa8c16225cec7fa6687
Marvin Lee <marvinlee.github@gmail.com>
2023-06-26 22:01:16 -0700

    Added E2E testing for signing in for the first time

--- src/end-to-end-tests/login-and-sign-in.test.js
+++ src/end-to-end-tests/login-and-sign-in.test.js
@@ -13,8 +13,8 @@ beforeEach(() => {
 	app = appExports.default;
 });
 
-it("Create New User + Login", async () => {
-	// TODO: Consider using uuids for passwords for tests
+it("Log In + Sign In", async () => {
+	// TODO: Skip creating a user and use a pre-made user
 	const e2ePassword = "e2e-test-password";
 	const createRes = await request(app)
 		.post("/user")
@@ -40,4 +40,22 @@ it("Create New User + Login", async () => {
 
 	expect(loginRes.statusCode).toBe(200);
 	expect(loginRes.body).toContainKey("api_key");
+
+	const apiKey = loginRes.body.api_key;
+
+	const signInRes = await request(app)
+		.patch("/user/session/latest")
+		.set("Accept", "application/json")
+		.set("Content-Type", "application/json; charset=utf-8")
+		.set("Authorization", `Bearer ${apiKey}`)
+		.send({
+			method: "sign_in"
+		});
+
+	expect(signInRes.status).toBe(200);
+	expect(signInRes.body.ok).toBeTrue();
+	expect(signInRes.body).toContainKey("session")
+	expect(signInRes.body.session).toContainKeys(["startTime", "endTime"])
+	expect(signInRes.body.session.startTime).toBeGreaterThan(0);
+	expect(signInRes.body.session.endTime).toBeNull();
 });
