8734e113c7c05aa61b73eb67a8e7518c1ce6500c
Marvin Lee <marvinlee.github@gmail.com>
2024-01-28 23:59:13 -0800

    Added MySQL setup steps

---
+++ setup/setup-mysql.sql
@@ -0,0 +1,19 @@
+-- Sample commands for creating the database used by the project
+-- IMPORTANT: Before copy-pasting, make the following substitutions manually or with Find-and-Replace (without the quotes):
+    -- INSERT_DATABASE_NAME_HERE - Replace with 'timesheet_server' or a custom name for the MySQL database
+    -- INSERT_API_USER_NAME_HERE - Replace with 'timesheet-api' or a custom MySQL username for the project to use
+    -- INSERT_API_USER_PASSWORD_HERE - Replace with 'The-password-4-the-timesheet-api-user' or a custom password for the MySQL user
+
+
+-- Run the following as a MySQL superuser like `mysql` or `root` to create the database
+CREATE DATABASE `INSERT_DATABASE_NAME_HERE`;
+
+-- Run the following to create the new MySQL user
+CREATE USER 'INSERT_API_USER_NAME_HERE'@'localhost' IDENTIFIED BY 'INSERT_API_USER_PASSWORD_HERE';
+
+-- Add permissions to:
+    -- Create/Delete Tables
+    -- Select from Tables
+    -- Insert to Tables
+    -- Update to Tables
+    -- Delete from Tables
